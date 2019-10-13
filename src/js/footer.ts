5f81c55dcc0da6d760121a945722b34d02ed9f48
Tim <truecarry@gmail.com>
2019-10-13 10:58:06 +0300

    Fix wrong indent

--- src/js/footer.ts
+++ src/js/footer.ts
@@ -7,32 +7,32 @@ import { SupportersDialog } from './supporters'
 let tutorial: TutorialDialog
 
 export function showTutorial() {
-  tutorial.show()
+    tutorial.show()
 }
 
 export function doFooterThings() {
-  tutorial = new TutorialDialog()
-  console.log('doc body', document, document.body)
-  document.body.appendChild(tutorial.container)
-  document.getElementById('link-help').addEventListener('click', () => {
-    showTutorial()
-  })
+    tutorial = new TutorialDialog()
+    console.log('doc body', document, document.body)
+    document.body.appendChild(tutorial.container)
+    document.getElementById('link-help').addEventListener('click', () => {
+        showTutorial()
+    })
 
-  let whatsNew = new WhatsNewDialog()
-  document.getElementById('link-whatsNew').addEventListener('click', () => {
-    whatsNew.show()
-  })
-  document.body.appendChild(whatsNew.container)
+    let whatsNew = new WhatsNewDialog()
+    document.getElementById('link-whatsNew').addEventListener('click', () => {
+        whatsNew.show()
+    })
+    document.body.appendChild(whatsNew.container)
 
-  document.getElementById('closeFooter').addEventListener('click', () => {
-    document.getElementById('container').style.height = '100%'
-    hideElement(document.getElementById('footer'))
-    GlobalEvents.emitEvent('resize')
-  })
+    document.getElementById('closeFooter').addEventListener('click', () => {
+        document.getElementById('container').style.height = '100%'
+        hideElement(document.getElementById('footer'))
+        GlobalEvents.emitEvent('resize')
+    })
 
-  let supporters = new SupportersDialog()
-  document.getElementById('link-supporters').addEventListener('click', () => {
-    supporters.show()
-  })
-  document.body.appendChild(supporters.container)
+    let supporters = new SupportersDialog()
+    document.getElementById('link-supporters').addEventListener('click', () => {
+        supporters.show()
+    })
+    document.body.appendChild(supporters.container)
 }

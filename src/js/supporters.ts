b4e27b931c4ebe358c76df3e9ffc78418ce26076
Mina Kemp <aliceminacarter@gmail.com>
2020-08-12 20:33:49 -0500

    Add patron

--- src/js/supporters.ts
+++ src/js/supporters.ts
@@ -1,7 +1,12 @@
 import { ClosableDialog } from "./closabledialog";
 
 export class SupportersDialog extends ClosableDialog {
-    private ppl =
+    private patrons =
+        [
+            "henby"
+        ];
+
+    private donors =
         [
             "Glen Cathey",
             "Betty Glez",
@@ -24,7 +29,7 @@ export class SupportersDialog extends ClosableDialog {
         this.innerHTML = "<h1>Supporters</h1>";
 
         this.innerHTML += "Thanks to these people for donating and contributing to my work!!<br><br>";
-        this.innerHTML += this.ppl.join("<br>");
+        this.innerHTML += this.patrons.concat(this.donors).join("<br>");
 
         this.innerHTML += "<br><br>I always ask before publishing a name so just let me know if you'd prefer to stay anonymous!<br>ok thx love u";
     }
